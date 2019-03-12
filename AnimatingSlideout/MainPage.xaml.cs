45e55ecbe14f02531520ef2e5842e2cf2db09276
Noel Barrios <noel.barrios@optum.com>
2019-03-12 14:06:57 -0500

    create a cooridinated animation of a button and scrollview

--- AnimatingSlideout/MainPage.xaml.cs
+++ AnimatingSlideout/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,30 +12,45 @@ namespace AnimatingSlideout
 {
     public partial class MainPage : ContentPage
     {
+        bool isSlideOutInView;
+
         public MainPage()
         {
+
             InitializeComponent();
         }
 
         async void OnAddNewBtnClicked(object sender, EventArgs args)
-        {
-            if (slideOutContentView.IsVisible == true)
+        {        
+            if ( isSlideOutInView == false)
             {
-                await slideOutContentView.TranslateTo(1500, 0, 1800);
-                slideOutContentView.IsVisible = false;
-
+                addNewbtn.IsEnabled = false;
+                // animate the content to its original position (defined in corresponding xaml file)
+                addNewbtn.TranslateTo(-(slideOutScrollView.Width - addNewbtn.Width), 0, 1000);
+                await slideOutScrollView.TranslateTo(0, 0, 1000);
+                isSlideOutInView = true;
+                addNewbtn.IsEnabled = true;
             }
             else
             {
-                slideOutContentView.IsVisible = true;
-                await slideOutContentView.TranslateTo(100, 0, 1800);
+                addNewbtn.IsEnabled = false;
+                // animate the content out of view
+                addNewbtn.TranslateTo(0, 0, 1000);
+                await slideOutScrollView.TranslateTo(slideOutScrollView.Width, 0, 1000);
+                isSlideOutInView = false;
+                addNewbtn.IsEnabled = true;
             }
+
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            Abs.TranslationX = 500;
+            //screenWidth = Application.Current.MainPage.Width;
+            // set the content out of the bounds of the screen
+            slideOutScrollView.TranslationX = (slideOutScrollView.Width);
+            isSlideOutInView = false;
+
         }
 
     }
