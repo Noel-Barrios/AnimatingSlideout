9c48464bd54da7be537fcb76ec1b104e6cde9f9a
Noel Barrios <noel.barrios@optum.com>
2019-03-12 10:29:37 -0500

    add to github

---
+++ AnimatingSlideout/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+
+namespace AnimatingSlideout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnAddNewBtnClicked(object sender, EventArgs args)
+        {
+            if (slideOutContentView.IsVisible == true)
+            {
+                await slideOutContentView.TranslateTo(1500, 0, 1800);
+                slideOutContentView.IsVisible = false;
+
+            }
+            else
+            {
+                slideOutContentView.IsVisible = true;
+                await slideOutContentView.TranslateTo(100, 0, 1800);
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Abs.TranslationX = 500;
+        }
+
+    }
+}
